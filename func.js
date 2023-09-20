2840f3b1f82ef6a74ac8df131644621c30e6abba
StudentAM <studentam@space.metroed.local>
2023-09-20 08:54:54 -0700

    m

--- func.js
+++ func.js
@@ -11,3 +11,11 @@ function openNavbar() {
 	const navMenu = document.getElementById("nav-menu");
 	navMenu.classList.toggle("active");
 }
+function classToggle() {
+    const navs = document.querySelectorAll('.Navbar__Items')
+    
+    navs.forEach(nav => nav.classList.toggle('Navbar__ToggleShow'));
+  }
+  
+  document.querySelector('.Navbar__Link-toggle')
+    .addEventListener('click', classToggle);
